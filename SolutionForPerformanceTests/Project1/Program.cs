f740138c815f44e377a300bcaa5e89177f50965c
Tamir Levi <tamir@apiiro.com>
2023-12-12 17:34:22 +0200

    asdf

---
+++ SolutionForPerformanceTests/Project1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Project2;
+
+var a = new Class1();
+
+Console.WriteLine("Hello, World!");
---
+++ SolutionForPerformanceTests/Project2/Directory1/Class2.cs
@@ -0,0 +1,5 @@
+namespace Project2.Directory1;
+
+public class Class2
+{
+}
